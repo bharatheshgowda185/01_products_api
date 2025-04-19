e04235343cbf5f36d4d2856e0b50ca27f3221542
Ashok <ashokitschool@gmail.com>
2025-04-19 10:05:53 +0530

    jira-102 completed

--- src/main/java/in/ashokit/ecomm/EcommerceBackendApplication.java
+++ src/main/java/in/ashokit/ecomm/EcommerceBackendApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommerceBackendApplication {
+	
+	// JIRA - 102
+	public void m2(){
+		
+			// logic
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceBackendApplication.class, args);
